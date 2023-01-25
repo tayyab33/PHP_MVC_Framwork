b50d24884d80880b84f97d948a8cf0d4f712ba3d
Tayyab Ali <mushtaqahmeda680@gmail.com>
2023-01-25 06:06:27 +0500

    working with register.php

--- views/register.php
+++ views/register.php
@@ -2,8 +2,7 @@
 	<h1>Create an Account</h1>
     <?php 
        $form = \app\core\form\Form::begin('', "post"); ?>
-       <?php echo $form->field($model, 'firstname') ?>
+       <?php echo $form->field(\app\core\Model , 'firstname') ?>
       <input type="submit" name="submit">
     <?php echo \app\core\form\Form::end(); ?>
-
 </div>
